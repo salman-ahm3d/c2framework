8630bc7cfab6ee137b4cf04e8f7146fbd6e0a28b
Salman Ahmed <salmanahmed69890@gmail.com>
2025-01-24 15:47:41 +0500

    Add GET and POST functionality for tasks

--- resources.py
+++ resources.py
@@ -10,9 +10,36 @@ class Tasks(Resource):
     # ListTasks
     def get(self):
         # Add behavior for GET here
-        return "GET Success!", 200
+        tasks = Task.objects().to_json()
+        return Response(tasks, mimetype="application/json", status=200)
     
     # AddTasks
     def post(self):
         # Add behavior for POST here
-        return "POST Success!", 200
+        # Parse out JSON body we want to add to database
+        body = request.get_json()
+        json_obj = json.loads(json.dumps(body))
+
+        # Get the number of Task objects in the request
+        obj_num = len(body)
+
+        # For each task object, add it to the database
+        for i in range(len(body)):
+            # Add a task UUID to each task object for tracking
+            json_obj[i]['task_id'] = str(uuid.uuid4())
+
+            # Save Task object to database
+            Task(**json_obj[i]).save()
+
+            # Load the options provided for the task into an array for tracking in history
+            task_options = []
+            for key in json_obj[i].keys():
+                # Anything that comes after task_type and task_id is treated as an option
+                if (key != "task_type" and key != "task_id"):
+                    task_options.append(key + ": " + json_obj[i][key])
+        
+        # Return the last Task objects that were added
+        return Response(Task.objects.skip(Task.objects.count() - obj_num).to_json(),
+                        mimetype="application/json",
+                        status=200)
+
