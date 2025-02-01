deeea7f82d213f0ffc92572550f475a323688136
Salman Ahmed <salmanahmed69890@gmail.com>
2025-02-01 15:15:55 +0500

    Add C++ code for Raindoll implant

---
+++ implant-code/results.cpp
@@ -0,0 +1,22 @@
+//results.cpp
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ *
+ * The original author of this code is Josh Lospinoso (@jalospinoso).
+ * The unmodified source code can be found here: https://github.com/JLospinoso/cpp-implant
+*/
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include "results.h"
+
+
+// Result object returned by all tasks
+// Includes the task ID, result contents and success status (true/false)
+Result::Result(const boost::uuids::uuid& id,
+	std::string contents,
+	const bool success)
+	: id(id), contents{ std::move(contents) }, success(success) {
+}
---
+++ implant-code/results.h
@@ -0,0 +1,24 @@
+//results.h
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this source code package.
+ *
+ * The original author of this code is Josh Lospinoso (@jalospinoso).
+ * The unmodified source code can be found here: https://github.com/JLospinoso/cpp-implant
+*/
+#pragma once
+
+#define _SILENCE_CXX17_C_HEADER_DEPRECATION_WARNING
+
+#include <string>
+#include <boost/uuid/uuid.hpp>
+
+// Define our Result object
+struct Result {
+	Result(const boost::uuids::uuid& id,
+		std::string contents,
+		bool success);
+	const boost::uuids::uuid id;
+	const std::string contents;
+	const bool success;
+};
